33cd84ef0f41fb9a5772d02dd4816dd5d3b0c0da
racztamara97 <racztamara97@gmail.com>
2018-07-19 09:36:03 +0200

    refactoring

--- src/main/java/com/code/cool/racztami/Main.java
+++ src/main/java/com/code/cool/racztami/Main.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -27,17 +28,27 @@ public class Main {
             Class MyClass = MyRoutes.class;
             Method[] methods = MyClass.getDeclaredMethods();
 
+            String response = "Path not found " + requestedPath;
+
             for (Method method : methods) {
                 if (method.getAnnotation(WebRoute.class) != null) {
                     WebRoute annotation = method.getAnnotation(WebRoute.class);
                     if (annotation.path().equals(requestedPath)) {
                         try {
-                            method.invoke(new MyRoutes(), t);
+                            response = (String) method.invoke(new MyRoutes(), t);
                         } catch (IllegalAccessException | InvocationTargetException e) {
                             e.printStackTrace();
                         }
                     }
                 }
+            }
+
+            try {
+                t.sendResponseHeaders(200, response.getBytes().length);
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- src/main/java/com/code/cool/racztami/MyRoutes.java
+++ src/main/java/com/code/cool/racztami/MyRoutes.java
@@ -1,34 +1,21 @@
 package com.code.cool.racztami;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import com.code.cool.racztami.WebRoute;
 import com.sun.net.httpserver.HttpExchange;
 
 public class MyRoutes{
 
-    private void responseWriter(HttpExchange t, String response) throws IOException {
-        t.sendResponseHeaders(200, response.getBytes().length);
-        OutputStream os = t.getResponseBody();
-        os.write(response.getBytes());
-        os.close();
-    }
-
     @WebRoute(path = "/")
-    void index(HttpExchange t) throws IOException {
-        String response = "Index page";
-        responseWriter(t, response);
+    String index(HttpExchange t) {
+        return "Index page";
     }
 
     @WebRoute(path = "/test1")
-    void test1(HttpExchange t) throws IOException {
-        String response = "This is my first test";
-        responseWriter(t, response);
+    String test1(HttpExchange t) {
+        return "This is my first test";
     }
 
     @WebRoute(path = "/test2")
-    void test2(HttpExchange t) throws IOException {
-        String response = "This is my second test";
-        responseWriter(t, response);
+    String test2(HttpExchange t) {
+        return "This is my second test";
     }
 }
